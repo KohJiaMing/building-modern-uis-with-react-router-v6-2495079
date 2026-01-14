d8bf88f7384953ab3ab18915d32e3287db81b2d0
KohJiaMing <KohJiaMing4578@gmail.com>
2026-01-14 14:18:40 +0800

    L17

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,17 @@
+import { Outlet, Link } from "react-router-dom";
+import { getCategories } from "../api";
 export default function Categories() {
+  const categories = getCategories();
   return (
     <div className="container">
       <h1>Session Categories</h1>
+
+      <ul className="categories">
+        {categories.map((category) => (
+          <li key={category.id}><Link to={category.id}>{category.name}</Link></li>
+        ))}
+      </ul>
+      <Outlet />
     </div>
   );
 }
